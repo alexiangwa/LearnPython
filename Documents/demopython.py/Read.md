53a2e6af644c866e1baf3b5c4d8a8c36a31b544d
alexia ngwa <achaalexiangwa@yahoo.com>
2022-11-12 23:31:31 -0600

    python scripts

---
+++ Documents/demopython.py/Read.md
@@ -0,0 +1,5 @@
+to see the version of your python script run the following command
+
+import sys
+print(sys.version) 
+ click ctrl + shift +b to run
---
+++ Documents/demopython.py/learnpython.py
@@ -0,0 +1,8 @@
+print ("welcome to python 3.x")
+
+import sys
+print(sys.version)
+
+print("Hello World")
+print("Welcome to Python")
+print("my first python script")
