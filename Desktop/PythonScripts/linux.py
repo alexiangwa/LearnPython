34b01b977b457ad412cadec528de08ab799e8d8e
alexia ngwa <achaalexiangwa@yahoo.com>
2022-11-18 22:55:06 -0600

    updates

--- Desktop/PythonScripts/linux.py
+++ Desktop/PythonScripts/linux.py
@@ -1,5 +1,7 @@
-/c/Users/American/AppData/Local/Programs/Python/Python38/python
+#!/c/Users/American/AppData/Local/Programs/Python/Python38/python
+ World")
 print("Hello World")
 print("learn python")
 print("consistency is key")
 print("this is my first script")
+~
